86a0b31cf04eec80fbde310709b3e8a80630a5f7
f47fr0g@gmail.com <f47fr0g@gmail.com>
2023-05-23 22:27:46 +0530

    Camrl camrl

---
+++ index.js
@@ -0,0 +1,23 @@
+import * as mod from "/sha512.min.js";
+
+function makepss() {
+  var text = sha512(document.getElementById("psw").value);
+  var value = '';
+  for (var i = 0; i < text.length; i++) {
+    if (i % 8 == 0) {
+      value = value + text[i];
+    }
+  }
+  document.getElementById("pswd").innerHTML = value + "@A";
+}
+
+document.getElementById("makepsbtn")
+  .addEventListener("click", makepss, false);
+
+function copyText() {
+  var copyText = document.getElementById("pswd").innerHTML;
+  navigator.clipboard.writeText(copyText);
+}
+
+document.getElementById("copybtn")
+  .addEventListener("click", copyText, false);
