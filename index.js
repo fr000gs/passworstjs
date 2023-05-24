a64b072028fa7bd885d2e64c3034e10e115164a3
f47fr0g@gmail.com <f47fr0g@gmail.com>
2023-05-24 17:14:21 +0530

    fr000gs

--- index.js
+++ index.js
@@ -1,7 +1,114 @@
 import * as mod from "./sha512.min.js";
+//import {AES} from "./aes.js";
+import { fromByteArray, toByteArray, byteLength }
+from "./b64.js";
+
+import { ECB } from "./ecb.js";
+
+var encoder = new TextEncoder();
+var decoder = new TextDecoder();
+
+function aesecbenc(text, key) {
+  //var key = encoder.encode("Example128BitKey");
+  // Convert text to bytes
+  //var text = 'TextMustBe16Byte';
+  var textBytes = encoder.encode(text);
+
+  var aesEcb = new ECB(key);
+  var encryptedBytes = new Uint8Array(textBytes.length);
+  aesEcb.encrypt(textBytes, encryptedBytes);
+  return fromByteArray(encryptedBytes);
+}
+
+function aesecbdec(encryptedBytes, key) {
+  var aesEcb = new ECB(key);
+  var decryptedBytes = new Uint8Array(encryptedBytes.length);
+  aesEcb.decrypt(encryptedBytes, decryptedBytes);
+
+  // Convert our bytes back into text
+  var decryptedText = decoder.decode(decryptedBytes);
+  return decryptedText;
+}
+
+function longtext(text) {
+  var chunksize = 6;
+  var texts = text.match(/[\s\S]{1,16}/g) || [' '];
+  var poop = texts.pop();
+  texts.push(poop.padEnd(16));
+  return texts;
+}
+
+function cut16(text) {
+  var te = '';
+  for (var i = 0;
+    (i < 16) && (i < text.length); i++) {
+    te = te + text[i];
+  }
+  return te.padEnd(16);
+}
+
+function encbtns() {
+  var text = longtext(document
+    .getElementById("inputtext").value);
+  var key = cut16(document
+    .getElementById("key").value);
+  var endo = "";
+  for (var i = 0; i < text.length; i++) {
+    endo = endo + aesecbenc(text[i], key) + '@';
+  }
+  document.getElementById("encdec")
+    .innerHTML = endo;
+}
+
+document.getElementById("encbtn")
+  .addEventListener("click", encbtns, false);
+
+function cutter(text) {
+  var texts = [];
+  var chunk = '';
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == '@') {
+      texts.push(chunk);
+      chunk = '';
+    }
+    else {
+      chunk = chunk + text[i];
+    }
+  }
+  if (chunk) { texts.push(chunk); } //last @
+  return texts;
+}
+
+function decbtns() {
+  var text = cutter(document
+    .getElementById("inputtext").value);
+  var key = cut16(document
+    .getElementById("key").value);
+  var bytes = '';
+  for (var i = 0; i < text.length; i++) {
+    bytes = bytes + aesecbdec(
+      toByteArray(text[i]), key);
+  }
+  document.getElementById("encdec")
+    .innerHTML = bytes;
+}
+
+document.getElementById("decbtn")
+  .addEventListener("click", decbtns, false);
+
+function copyOutput() {
+  navigator.clipboard.writeText(
+    document.getElementById("encdec").value)
+}
+
+document.getElementById("copyoutput")
+  .addEventListener("click", copyOutput, false);
+
+
+/////////////////////////////////
 
 function makepss() {
-  var text = sha512(document.getElementById("psw").value + document.getElementById("usr").value );
+  var text = sha512(document.getElementById("psw").value + document.getElementById("usr").value);
   var value = '';
   for (var i = 0; i < text.length; i++) {
     if (i % 8 == 0) {
@@ -14,10 +121,10 @@ function makepss() {
 document.getElementById("makepsbtn")
   .addEventListener("click", makepss, false);
 
-function copyText() {
+function copyPassword() {
   var copyText = document.getElementById("pswd").innerHTML;
   navigator.clipboard.writeText(copyText);
 }
 
 document.getElementById("copybtn")
-  .addEventListener("click", copyText, false);
+  .addEventListener("click", copyPassword, false);
